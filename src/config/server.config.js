289d61d3eb7983ff1b564cdb21072c8c7689a1be
Yogesh Siwan <yogesh.siwan@grid-os.com>
2025-04-22 06:28:59 +0530

    ...

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3001,
   cors: {
-    origin: ['http://localhost:3000','https://metagross-nu.vercel.app/'],
+    origin: ['http://localhost:3000','https://metagross-nu.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
 }; 
+
+
